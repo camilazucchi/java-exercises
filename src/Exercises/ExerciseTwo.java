e7f3e686f5453479ccf6123735fd11c37154bdca
Camila Zucchi <camilazucchi@outlook.com>
2023-05-05 13:31:28 -0300

    feat: add second exercise (sum, subtraction, multiplication and division of two numbers)

---
+++ src/Exercises/ExerciseTwo.java
@@ -0,0 +1,29 @@
+package Exercises;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        // 2. Faça um algoritmo que receba dois números e ao final mostre a soma, subtração, multiplicação e a divisão
+        //dos dois números lidos.
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        try (Scanner scanner = new Scanner(System.in)) {
+        System.out.println("Digite o primeiro número.");
+        double firstNumber = scanner.nextDouble();
+        System.out.println("Digite o segundo número.");
+        double secondNumber = scanner.nextDouble();
+
+        double sumOfNumbers = firstNumber + secondNumber;
+        double subtractionOfNumbers = firstNumber - secondNumber;
+        double multiplicationOfNumbers = firstNumber * secondNumber;
+        double divisionOfNumbers = firstNumber / secondNumber;
+
+        System.out.println("A soma é: " + df.format(sumOfNumbers) + ".");
+        System.out.println("A subtração é: " + df.format(subtractionOfNumbers) + ".");
+        System.out.println("A multiplicação é: " + df.format(multiplicationOfNumbers) + ".");
+        System.out.println("A divisão é: " + df.format(divisionOfNumbers) + ".");
+        }
+    }
+}
